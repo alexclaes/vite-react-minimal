7708a45843fcda73d37fab23ed250975c071d986
Alex Claes <hello@aclaes.com>
2024-05-30 11:28:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <h1 className="App-headline">Vite React Minimal Template</h1>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test.
+      </p>
+    </>
+  );
+}
