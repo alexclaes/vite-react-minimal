081e97d56f9a068be066316e9fe324f190bea02e
Alex Claes <hello@aclaes.com>
2024-06-18 15:35:37 +0200

    add wrapper div

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './App.css';
 
 export default function App() {
   return (
-    <>
+    <div className="App">
       <h1 className="App-headline">Vite React Minimal Template</h1>
       <p>
         Edit <code>src/App.jsx</code> and save to test.
       </p>
-    </>
+    </div>
   );
 }
